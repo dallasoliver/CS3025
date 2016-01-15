3c3ca16123a049229c0c7849321b523cbff20881
dallasoliver <dallasoliver@live.ca>
2016-01-15 13:00:42 -0400

    Login screen logic start

--- Senior Student Alliance/src/ui/Interface.java
+++ Senior Student Alliance/src/ui/Interface.java
@@ -1,20 +1,23 @@
 package ui;
 
-import javax.swing.JPanel;
-
 import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JLabel;
-import javax.swing.JButton;
-import javax.swing.SwingUtilities;
-
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
 
 public class Interface extends JFrame{
 	
@@ -22,10 +25,12 @@ public class Interface extends JFrame{
 	private JPanel cardPanel;
 	private JPanel loginPanel;
 	private JPanel viewPostingsPanel;
+	private JPanel adminPanel;
 	private JTextField textFieldUsername;
 	private JTextField textFieldPassword;
 	private JLabel lblUsername;
 	private JLabel lblPassword;
+	private JLabel lblInvalidLogin;
 	private CardLayout cardLayout;
 	private JButton btnSubmit;
 	private JLabel picLabel;
@@ -39,61 +44,100 @@ public class Interface extends JFrame{
 		cardPanel = new JPanel();
 		loginPanel = new JPanel();
 		viewPostingsPanel = new JPanel();
+		adminPanel = new JPanel();
 		textFieldUsername = new JTextField(10);
 		textFieldPassword = new JTextField(10);
 		lblUsername = new JLabel("Username");
 		lblPassword = new JLabel("Password");
+		lblInvalidLogin = new JLabel("Invalid Login");
+		lblInvalidLogin.setVisible(false);
 		cardLayout = new CardLayout();
 		btnSubmit = new JButton("Submit");
+		picLabel = new JLabel("");
+		
+		//login start
+		Image img = new ImageIcon(this.getClass().getResource("/image1.png")).getImage();
+		picLabel.setIcon(new ImageIcon(img));
+		picLabel.setBounds(10, 53, 166, 266);
 		
 		cardPanel.setLayout(cardLayout);
-		viewPostingsPanel.setLayout(null);
 		loginPanel.setLayout(new GridBagLayout());
+		
 		GridBagConstraints constraints = new GridBagConstraints();
 		
-		constraints.anchor = GridBagConstraints.LINE_END;
+		constraints.anchor = GridBagConstraints.CENTER;
 		constraints.weightx = 0.5;
 		constraints.weighty = 0.5;
 		
+		constraints.gridwidth = 3;
+
 		constraints.gridx = 0;
 		constraints.gridy = 0;
 		loginPanel.add(picLabel, constraints);
 		
-		constraints.gridx = 0;
+		constraints.gridwidth = 1;
+				
+		constraints.anchor = GridBagConstraints.LINE_END;
+		constraints.gridx = 1;
 		constraints.gridy = 1;
 		loginPanel.add(lblUsername, constraints);
 		
-		constraints.gridx = 0;
+		constraints.gridx = 1;
 		constraints.gridy = 2;
 		loginPanel.add(lblPassword, constraints);
 		
 		constraints.anchor = GridBagConstraints.LINE_START;
-		constraints.gridx = 1;
+		constraints.gridx = 2;
 		constraints.gridy = 1;
 		loginPanel.add(textFieldUsername, constraints);
 		
-		constraints.gridx = 1;
+		constraints.gridx = 2;
 		constraints.gridy = 2;
 		loginPanel.add(textFieldPassword, constraints);
 		
+		btnSubmit.setSize(10, 20);
+		
 		constraints.anchor = GridBagConstraints.FIRST_LINE_START;
 		constraints.weighty = 10;
-		constraints.gridx = 1;
+		constraints.gridx = 2;
 		constraints.gridy = 3;
 		loginPanel.add(btnSubmit, constraints);
 		
+		constraints.gridx = 3;
+		constraints.gridy = 3;
+		loginPanel.add(lblInvalidLogin, constraints);
+				
+		loginPanel.setBackground(Color.lightGray);
+		
 		cardPanel.add(loginPanel, "1");
+		//login end
+
+		viewPostingsPanel.setLayout(new GridBagLayout());
+		adminPanel.setLayout(new GridBagLayout());
 		
 		cardPanel.add(viewPostingsPanel, "2");
-		
+
+		cardPanel.add(adminPanel, "3");
+
 		cardLayout.show(cardPanel, "1");
 
 		btnSubmit.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				cardLayout.show(cardPanel, "2");
+				if (textFieldUsername.getText().equals("admin") && textFieldPassword.getText().equals("admin")) {
+					cardLayout.show(cardPanel, "3");					
+				} else if ((textFieldUsername.getText().equals("senior") && textFieldPassword.getText().equals("senior")) || (textFieldUsername.getText().equals("student") && textFieldPassword.getText().equals("student"))) {
+					cardLayout.show(cardPanel, "2");					
+				} else {
+					lblInvalidLogin.setVisible(true);
+				}
 			}
 		});
+		
+		try {
+		    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+		}
 		
 		frame.getContentPane().add(cardPanel);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
